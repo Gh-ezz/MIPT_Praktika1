7ccb88d52b416ba16686aa3727fe8d559aa45509
Gh-ezz <kipraszitkus@gmail.com>
2025-09-22 14:48:40 +0300

    Hello World veikia ant emuliatoriaus

--- app/src/main/java/com/example/mipt_praktika1/MainActivity.java
+++ app/src/main/java/com/example/mipt_praktika1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mipt_praktika1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMessage;
+    private Button btnShowText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,18 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // susiejame view'us
+        tvMessage = findViewById(R.id.tvMessage);
+        btnShowText = findViewById(R.id.btnShowText);
+
+        // paspaudus mygtuką – pakeisti tekstą
+        btnShowText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvMessage.setText(getString(R.string.hello));
+            }
         });
     }
 }
